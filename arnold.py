03b97631c362caef353bb05c5e111b42b2c59da8
Deborah Venuti <deborahvenuti93@gmail.com>
2017-04-22 17:33:13 -0400

    Basic main program setup - not much in there yet.

---
+++ arnold.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from auth import Twitter_Auth, Reddit_Auth, Instagram_Auth
+from twitter_module import *
+from reddit_module import * 
+
+# Have some sort of user menu to let the user pick which accounts
+# they want to authorize. 
+
+
+# Twitter Authorization
+#_twitter = Twitter_Auth()
+#twitter_api = _twitter.get_authorized_user()
+
+# Instagram Authorization
+#_instagram = Instagram_Auth()
+#instagram_api = _instagram.get_authorized_user()
+
+# Reddit Authorization
+#_reddit = Reddit_Auth()
+#reddit_api = _reddit.get_authorized_user()
+
+#Call get_timeline(twitter_api) to get the users twitter timeline
+#Call TJs function to display user feed and pass instagram_api as a parameter
+#Call katies print_timeline(reddit_api) to display users reddit timeline
